666ded4d3d5091679f791e21f27a3ad4fba02c29
AbbyToto <nrty11@gmail.com>
2023-09-01 16:53:52 -0400

    update index

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ $peter = new Personne();
 $peter ->setName("Peter c'est une personne");
 echo $peter -> getName();
 
-
-
-
 ?>
+// changes cursor -> index.php ->+
+// git commit -m "creation class personne et affichage sur index"
+// git add .
+// git pull
+// git push
