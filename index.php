c675758b846dee7447d57e226dfeabe352b07e2c
AbbyToto <143623892+AbbyToto@users.noreply.github.com>
2023-09-27 09:57:59 -0400

    update index ,classe_livre

--- index.php
+++ index.php
@@ -1,13 +1,46 @@
 <?php
-include_once "Person.php";
-$peter = new Personne();
+$host = "localhost";
+$db = "31b";
+$user = "root";
+$password = "";
+$dsn = "mysql:host=$host;dbname= $db;charset=UTF8";
+global $oPDO;
+try{
+    $oPDO = new PDO($dsn, $user, $password);
+    if($oPDO){
+        echo "Conneted to the $db database successfully";
+    }
 
-$peter ->setName("Peter c'est une personne");
-echo $peter -> getName();
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+require_once "class/Livre.php";
+
+
+$olivre = new livre;
+$livre = $olivre->getLivre
+var_dump($olivre);
+echo "<br>";
+echo "<br>";
+
+
+// $livres = $olivre->getLivres("2");
+// echo "<br>";
+// echo "<br>";
+
+// print_r ($livres);
+// var_dump($livres);
+// echo "<br>";
+// var_dump($livres[1]);
+
+// echo "<br>";
+// echo "<br>";
+// $livre = $oli
+
+$olivre = new Livre;
+
+
+
 
 ?>
-// changes cursor -> index.php ->+
-// git commit -m "creation class personne et affichage sur index"
-// git add .
-// git pull
-// git push
